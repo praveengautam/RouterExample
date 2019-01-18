ef5c6e9aca7812520c3061be47358d6efde6bfe5
praveen <praveengautam199619@gmail.com>
2019-01-18 18:53:02 +0530

    Sample router

---
+++ index.js
@@ -0,0 +1,10 @@
+/** @format */
+
+import {AppRegistry} from 'react-native';
+//import App from './App';
+import {name as appName} from './app.json';
+import ModalExample, {} from './src/ModalExample'
+import Routes from './src/Routes.js';
+
+//AppRegistry.registerComponent(appName, () => ModalExample);
+AppRegistry.registerComponent(appName, () => Routes);
